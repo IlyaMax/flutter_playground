dd0a79729ce64a29f311c2f5ad9a16e108fb9382
maximencko <ilyamaximencko@gmail.com>
2023-04-12 14:50:08 +0300

    rollback main

--- lib/main.dart
+++ lib/main.dart
@@ -1,263 +1,115 @@
-import 'package:beamer/beamer.dart';
-import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 void main() {
-  final routerDelegate = BeamerDelegate(
-    initialPath: '/home',
-    locationBuilder: RoutesLocationBuilder(
-      routes: {'/home': (_, __, ___) => HomeScreen()},
-    ),
-  );
-  runApp(MaterialApp.router(
-    routeInformationParser: BeamerParser(),
-    routerDelegate: routerDelegate,
-  ));
+  runApp(const MyApp());
 }
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  // ignore: strict_raw_type
-  List<Tab> _tabs = [];
-  int _currentTabIndex = 0;
-  List<BeamerDelegate> _nestedRouterDelegates = [];
-  late BeamerDelegate _homeRouterDelegate;
-
-  @override
-  void initState() {
-    super.initState();
-    _currentTabIndex = 0;
-    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
-  }
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            child: Beamer(routerDelegate: _homeRouterDelegate),
-          ),
-          _BottomBar(
-            tabs: _tabs,
-            onTabPressed: (index) {
-              if (index == _currentTabIndex) return;
-              final destination = (index == 0)
-                  ? '/tab1'
-                  : (index == 1)
-                      ? '/tab2'
-                      : '/tab3';
-              _homeRouterDelegate.beamToNamed(destination);
-            },
-            currentTabIndex: _currentTabIndex,
-          ),
-        ],
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          _homeRouterDelegate.beamToNamed('/tab2');
-        },
-      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
 
   @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    if (_tabs.isEmpty) {
-      _tabs = [
-        Tab(
-          MockScreenLocation('tab1'),
-          Icons.book,
-          'tab1',
-        ),
-        Tab(
-          MockScreenLocation('tab2'),
-          Icons.article,
-          'tab2',
-        ),
-        Tab(
-          MockScreenLocation('tab3'),
-          Icons.cabin,
-          'tab3',
-        ),
-      ];
-      _nestedRouterDelegates = _tabs
-          .map((tab) => BeamerDelegate(locationBuilder: (routeInformation, _) {
-                return tab.rootLocation;
-              }))
-          .toList();
-      _homeRouterDelegate = BeamerDelegate(
-        initialPath: '/tab1',
-        locationBuilder: RoutesLocationBuilder(
-          routes: {
-            '/tab1': (context, state, _) =>
-                Beamer(routerDelegate: _nestedRouterDelegates[0]),
-            '/tab2': (context, state, _) =>
-                Beamer(routerDelegate: _nestedRouterDelegates[1]),
-            '/tab3': (context, state, _) =>
-                Beamer(routerDelegate: _nestedRouterDelegates[2]),
-          },
-        ),
-      );
-      _homeRouterDelegate.addListener(_updateCurrentTabIndex);
-    }
-  }
+  State<MyHomePage> createState() => _MyHomePageState();
+}
 
-  @override
-  void dispose() {
-    for (final delegate in _nestedRouterDelegates) {
-      delegate.dispose();
-    }
-    _homeRouterDelegate
-      ..removeListener(_updateCurrentTabIndex)
-      ..dispose();
-    super.dispose();
-  }
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
 
-  void _updateCurrentTabIndex() {
-    final currentLoc = _homeRouterDelegate.configuration.location;
+  void _incrementCounter() {
     setState(() {
-      _currentTabIndex = currentLoc == '/tab1'
-          ? 0
-          : currentLoc == '/tab2'
-              ? 1
-              : 2;
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
     });
   }
-}
-
-class _BottomBar extends StatelessWidget {
-  // ignore: strict_raw_type
-  final List<Tab> tabs;
-  final int currentTabIndex;
-  final void Function(int index) onTabPressed;
-  const _BottomBar({
-    Key? key,
-    required this.tabs,
-    required this.onTabPressed,
-    required this.currentTabIndex,
-  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      height: 50,
-      child: Row(
-        children: tabs
-            .mapIndexed(
-              (index, tab) => Expanded(
-                child: _BottomBarItem(
-                  isSelected: index == currentTabIndex,
-                  icon: tab.icon,
-                  title: tab.title,
-                  onPressed: () => onTabPressed(index),
-                ),
-              ),
-            )
-            .toList(),
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
       ),
-    );
-  }
-}
-
-class _BottomBarItem extends StatelessWidget {
-  final IconData icon;
-  final String title;
-  final bool isSelected;
-  final VoidCallback onPressed;
-  const _BottomBarItem({
-    Key? key,
-    required this.isSelected,
-    required this.icon,
-    required this.title,
-    required this.onPressed,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final color = isSelected ? Colors.blue[700] : Colors.blue;
-    return GestureDetector(
-      behavior: HitTestBehavior.opaque,
-      onTap: onPressed,
-      child: Center(
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
         child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Icon(icon, color: color),
-            const SizedBox(height: 3),
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
             Text(
-              title,
-              style: TextStyle(
-                fontWeight: FontWeight.w700,
-                fontSize: 10,
-                color: color,
-              ),
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
             ),
           ],
         ),
       ),
-    );
-  }
-}
-
-@immutable
-class Tab {
-  final BeamLocation rootLocation;
-  final IconData icon;
-  final String title;
-
-  const Tab(this.rootLocation, this.icon, this.title);
-}
-
-class MockScreenLocation extends BeamLocation<BeamState> {
-  final String title;
-
-  MockScreenLocation(this.title);
-  @override
-  List<Pattern> get pathPatterns => [RegExp(r'/.*mock\?title=.*')];
-  @override
-  List<BeamPage> buildPages(BuildContext context, BeamState state) {
-    return [BeamPage(child: MockScreen(title: title))];
-  }
-}
-
-class MockScreen extends StatefulWidget {
-  final String title;
-  const MockScreen({Key? key, required this.title}) : super(key: key);
-
-  @override
-  State<MockScreen> createState() => _MockScreenState();
-}
-
-class _MockScreenState extends State<MockScreen> {
-  late Future<String> stringFuture;
-  @override
-  void initState() {
-    super.initState();
-    stringFuture = Future.delayed(Duration(seconds: 1), () => widget.title);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: FutureBuilder<String>(
-          future: stringFuture,
-          builder: (context, snapshot) {
-            if (snapshot.data == null) return CircularProgressIndicator.adaptive();
-            return Text(widget.title);
-          }
-        ),
-      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
